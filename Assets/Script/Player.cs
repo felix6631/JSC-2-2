50f3255a2d48033f9cbed20ef64b564f27fd6c96
felix6631 <83746046+felix6631@users.noreply.github.com>
2021-11-05 09:18:56 +0900

    sprite applied
    bullet is kawaii

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -20,6 +20,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb2d;
     private BoxCollider2D bc2d;
     private Transform tf;
+    private SpriteRenderer sprd;
     public float jumpHeight = 6.0f;
     public float speed = 0.1f;
     private float moveX = 0;
@@ -31,6 +32,7 @@ public class Player : MonoBehaviour
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         bc2d = gameObject.GetComponent<BoxCollider2D>();
         tf = gameObject.GetComponent<Transform>();
+        sprd = gameObject.GetComponent<SpriteRenderer>();
     }
     // Start is called before the first frame update
     void Start()
@@ -56,8 +58,8 @@ public class Player : MonoBehaviour
 
     void moveInput()
     {
-        moveX = 0; speed = 0.1f;
-        if (Input.GetKeyDown(KeyCode.Space) && jumpActive) //jump
+        moveX = 0; speed = 0.05f;
+        if (Input.GetKeyDown(KeyCode.Z) && jumpActive) //jump
         {
             rb2d.velocity = new Vector2(0, jumpHeight);
             jumpActive = false;
@@ -66,13 +68,14 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         { //moveing left and right
             moveX = -1;
-            
+            sprd.flipX = true;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             moveX = 1;
+            sprd.flipX = false;
         }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && dashActive)
+        if (Input.GetKeyDown(KeyCode.C) && dashActive)
         {
             speed = 1.0f;
             dashActive = false;
